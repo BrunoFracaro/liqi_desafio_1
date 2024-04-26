ea02a7acc596d51aa973e6d48014b815100f9a50
BrunoAlcantara <bruno.fracaro.alcantara@gmail.com>
2024-04-26 15:45:31 -0300

    testing init setup

---
+++ src/test/api.test.js
@@ -0,0 +1,14 @@
+
+// TODO test all api endpoints
+
+describe('api', () => {
+
+  it('should be ok', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should be nok', () => {
+    expect(false).toBe(true);
+  });
+
+})
