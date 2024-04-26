63ac77f6fde52fcce4574f390eaea8b0c4769ec0
BrunoAlcantara <bruno.fracaro.alcantara@gmail.com>
2024-04-26 18:25:38 -0300

    convert function and test

--- src/test/funtions.test.js
+++ src/test/funtions.test.js
@@ -1,8 +1,18 @@
+import conversionRate from '../utils/conversionRate.js'
 
 // test utils function
 
 describe('function', () => {
 
-  it.todo('function should return correct conversion rate')
+  it('function should return correct conversion rate', () => {
+
+    const value = 100
+    const from = 1
+    const to = 2
+
+    const result = conversionRate(value, from, to)
+
+    expect(result).toBe(200)
+  })
 
 })
