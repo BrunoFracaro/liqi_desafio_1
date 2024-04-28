3ad32ec12368e3190af67a6ba0c8cd65c8f60a28
BrunoAlcantara <bruno.fracaro.alcantara@gmail.com>
2024-04-28 12:55:56 -0300

    part 2, async function

--- desafio-2-async/pass-function/app.mjs
+++ desafio-2-async/pass-function/app.mjs
@@ -1,27 +1,29 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+import AWS from 'aws-sdk';
+AWS.config.region = 'sa-east-1';
+
+const eventbridge = new AWS.EventBridge()
 
 export const lambdaHandler = async (event, context) => {
-  
+
   console.log('pass function triggered');
 
-  console.log(event);
+  let payload = { Entries: [] };
 
   for (const record of event.Records) {
 
     const body = JSON.parse(record.body);
-    console.log(body);
+
+    payload.Entries.push({
+      EventBusName: 'RecieveEventBus',
+      DetailType: 'Message',
+      Time: new Date(),
+      Source: 'demo.event',
+      Detail: JSON.stringify(body)
+    });
 
   }
 
+  const result = await eventbridge.putEvents(payload).promise();
+  console.log('EventBridge result');
+  console.log(result);
 };
